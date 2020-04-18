e837c6f3d9780d3dfe923895507ae219fa51903c
Priyanka Biswas <priyanka.biswas@recro.io>
2020-04-19 00:47:57 +0530

    Updates

---
+++ precache-manifest.6f182859142110833e4a1a1891753105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd4183d5895c1921cf6b971bf4efe9e",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "1ca8db569a7a9789df34",
+    "url": "/ShoppingCart/static/css/main.5ad7af8b.chunk.css"
+  },
+  {
+    "revision": "9277e2062a5bd08dddcd",
+    "url": "/ShoppingCart/static/js/2.7531987c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShoppingCart/static/js/2.7531987c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca8db569a7a9789df34",
+    "url": "/ShoppingCart/static/js/main.22baad46.chunk.js"
+  },
+  {
+    "revision": "3d377736dbb4f3ff6050",
+    "url": "/ShoppingCart/static/js/runtime-main.93a8c81c.js"
+  }
+]);
